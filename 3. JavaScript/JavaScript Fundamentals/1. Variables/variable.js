dbbb1b1d5233a69d0f36082514d2e193dce31297
imtiyaz-78 <imtiyazmd803@gmail.com>
2023-12-31 01:38:47 +0530

    conversion of datatypes in javaScript

---
+++ 3. JavaScript/JavaScript Fundamentals/1. Variables/variable.js
@@ -0,0 +1,28 @@
+
+const accounId = 12345
+let accountEmail = "imtiyazmd80.@gmail.com";
+var accountPassword = "786786";
+accountCity = "Darbhanga";
+let accountStates;
+
+
+// accounId = 3; // not allowed 
+
+accountEmail = "mutaz @806@gmail.com";
+accountPassword = "963852147";
+accountCity = "Bihar";
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+console.table([accounId, accountEmail, accountPassword, accountCity, accountStates]);
+
+
+
+
+
+
+
+
---
+++ 3. JavaScript/JavaScript Fundamentals/3. Conversion of dataTypes/conversionOperation.js
@@ -0,0 +1,66 @@
+
+//<=========> conversion In javaScript <==========>
+
+
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let scores = "33abc";
+// let valueInNumber = Number(scores)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+// let temp = null
+// let result = Number(temp);
+// console.log(typeof (result));
+// console.log(result);
+
+
+// let score = undefined;
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber)); // o/p: number
+// console.log(valueInNumber); // o/p:  NaN
+
+
+
+
+// let score = true;
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber)); // o/p: number
+// console.log(valueInNumber); // o/p:  1
+
+
+
+
+
+// let score = "Imtiyaz";
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber)); // o/p: number
+// console.log(valueInNumber); // o/p:  NaN
+
+
+// "786" ==> 33
+// "786abc" ==> NaN
+// true ==> 1; false ==> 0
+
+
+let isloggedIn = 1;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 ==> true: 0 => false
+// "" ==> false
+// "imtiyaz" ==> true
+
+
+
+let n = 786;
+let stringNumber = String(n)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+
+
