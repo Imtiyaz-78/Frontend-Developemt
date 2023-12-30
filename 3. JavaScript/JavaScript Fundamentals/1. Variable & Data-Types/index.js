4eab8a912b3e6e691bd804d3cfd103402ed19d62
imtiyaz-78 <imtiyazmd803@gmail.com>
2023-12-30 22:43:34 +0530

    variable & data-types

---
+++ 3. JavaScript/JavaScript Fundamentals/1. Variable & Data-Types/index.js
@@ -0,0 +1,17 @@
+
+// var a = "Hi";
+// var a  = "hoo"
+// console.log(a);
+// console.log(a);
+
+// let a = 10;
+//     a = 500;
+//     console.log(a);
+
+
+function test(){
+    const a = document.getElementsByName("h1").innerHTML ="HI"
+}
+
+
+
