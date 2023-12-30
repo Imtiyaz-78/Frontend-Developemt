8dab10e82a280f70f6785f1ee46dabd7dea3690d
imtiyaz-78 <imtiyazmd803@gmail.com>
2023-12-31 00:58:58 +0530

    dataTypes

---
+++ 3. JavaScript/JavaScript Fundamentals/2. Data Types/dataTypes.js
@@ -0,0 +1,39 @@
+// <=======> Data Types <========>
+// https://tc39.es/ecma262/  <--- Official Documnetation of JavaScript website 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript <--- mdn for developer
+
+"use strict" // treat all JS code as newer JS versions
+
+// alert(3 + 6) // we are using node JS, not Browser
+
+
+// ==> Data Types(Primitive):
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone vale (Ex: let states;) null means empty
+// undefined => variable that has no value assigned yet 
+// symbol => unique 
+
+/*
+JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+
+*/
+
+console.log(typeof null); // o/p: object <-- null is a object(Interview)
+console.log(typeof undefined); // o/p: undefined
+
+
+
+
+
+
