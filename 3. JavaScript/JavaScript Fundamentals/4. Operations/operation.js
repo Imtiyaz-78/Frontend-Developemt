e7ce9e68b858a23ac4e3f643a715b7acd4839c83
imtiyaz-78 <imtiyazmd803@gmail.com>
2024-01-01 00:34:59 +0530

    compresion in javaScript

---
+++ 3. JavaScript/JavaScript Fundamentals/4. Operations/operation.js
@@ -0,0 +1,67 @@
+
+// <============> Operation <=================>
+
+let value = 786;
+let negValue = -value;
+// console.log(`The value is ${value} and its negative is ${negValue}`);
+
+
+// console.log(2 + 3);
+// console.log(2 - 3);
+// console.log(2 * 3);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+
+
+// ==> String Operation
+
+let str1 = "md";
+let str2 = " imtiyaz";
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2); // ==> 12
+// console.log(1 + "2"); // ==> 12
+// console.log("1" + 2 + 2); // ==> 122
+// console.log(1 + 2 + "2"); // ==> 32
+// console.log("1" + (2 + 2)); // ==> 14
+
+
+// console.log(true); // ==> true
+// console.log(1 + true); // ==> 2
+// console.log(+true); // ==> 1
+// console.log(+""); // ==> 0
+// console.log(""); // ==> 0
+
+// ==> Tricky variable declaration:
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+// console.table([num1, num2, num3]); //==> (num1 , num2, num3 =  4 sabka )
+
+
+// ===> prefix increment  and postFix inCrement in JavaScript
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`); // o/p ==> x:4, y:3
+
+let n1 = 20;
+let n2 = n1++
+console.log(`n1:${n1}, n2:${n2}`); // o/p ==> n1: 21, n2: 20
+
+ // ==> final preFix and postFix
+//  Postfix increment
+let x1 = 3;
+const y1 = x++;
+// x1 is 4; y1 is 3
+
+// Prefix increment
+let x2 = 3;
+const y2 = ++x;
+// x2 is 4; y2 is 4
+
+
+
+
---
+++ 3. JavaScript/JavaScript Fundamentals/5. comparison of dataTypes/comparison.js
@@ -0,0 +1,22 @@
+
+
+// <==========> Comparison <===============>
+console.log("2" > 1); // ==> true, strin "2" ko number mein convert kar diya javaScript
+console.log(null > 0); // ==> false
+console.log(null == 0); // ==> false
+console.log(null >= 0); // ==> true , null ko javaScript zero(0) mein convert kar diya
+console.log("null end  ======>")
+
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+console.log(undefined <= 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined === 0); // false
+
+// Uper iss tarah ke compresion hum logo ko avoid karni chahiye
+
+
+//==> stric check ===
+console.log("4" === 4); // ==> false
