b60769e5b673f55b4996bd744ace7d07801d6dda
imtiyaz-78 <imtiyazmd803@gmail.com>
2024-01-08 19:48:57 +0530

    Array method

--- 3. JavaScript/JavaScript Fundamentals/6.  Arrays/array.js
+++ 3. JavaScript/JavaScript Fundamentals/6.  Arrays/array.js
@@ -256,9 +256,9 @@ console.log(myn1.slice(1, 3)); // [ 1, 2 ]
 console.log(myn1);  // [ 0, 1, 2, 3, 4, 5 ] (They are not changed in original array)
 
 
-console.log("Original Array B ==>", myn1);   // Original Array B ==> [ 0, 1, 2, 3, 4, 5 ]
-console.log(myn1.splice(1, 3));              // [ 1, 2, 3 ]
-console.log("Original Array changed ", myn1);// Original Array changed  [ 0, 4, 5 ]
+console.log("Original Array B ==>", myn1);    // Original Array B ==> [ 0, 1, 2, 3, 4, 5 ]
+console.log(myn1.splice(1, 3));               // [ 1, 2, 3 ]
+console.log("Original Array changed ", myn1); // Original Array changed  [ 0, 4, 5 ]
 
 
 
