cfe718c1f60356715313dbdaa42027c8d2d1c818
imtiyaz-78 <imtiyazmd803@gmail.com>
2024-01-14 23:01:46 +0530

    loops

--- 3. JavaScript/JavaScript Fundamentals/11. Loops in JS/loop.js
+++ 3. JavaScript/JavaScript Fundamentals/11. Loops in JS/loop.js
@@ -14,15 +14,19 @@
 
 */
 
+
 // for loop
+/*
 for (let i = 0; i <= 20; i++) {
     const element = i;
     console.log(element);
 
 }
 console.log("<=========>");
+*/
 
 
+/*
 const arr = [1, 2, 3, 4, 5, 6, 6, 8, 9, 10];
 
 for (let i = 0; i < arr.length; i++) {
@@ -36,9 +40,39 @@ for (let i = 0; i < arr.length; i++) {
         break;
     }
 }
+*/
+
+
+const arr = [10, 20, 30, 40, 50, 60, 70]
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 30) {
+        continue;
+    }
+    result = arr[i] +
+        // console.log(`Value of array: ${arr[i]}`);
+        console.log(arr[i]);
+
+}
+
 
 
 // 2. while loop
+
+
+
+
+/*
+// How to print result in single line 
+var result = "";
+var arr2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+for (var i = 1; i < arr2.length; i++) {
+    result = result + arr2[i] + " ";
+}
+console.log(typeof result, result)
+*/
+
+
 
 
 
