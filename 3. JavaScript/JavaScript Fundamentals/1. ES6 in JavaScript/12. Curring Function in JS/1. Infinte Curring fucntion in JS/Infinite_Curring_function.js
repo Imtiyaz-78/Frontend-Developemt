b43f78d72ae6a284511285a4c7c27bd4d9e66cd4
imtiyaz-78 <imtiyazmd803@gmail.com>
2024-02-11 18:28:15 +0530

    Infinte Curring fucntion discussed with real use  case

--- 3. JavaScript/JavaScript Fundamentals/1. ES6 in JavaScript/12. Curring Function in JS/1. Infinte Curring fucntion in JS/Infinite_Curring_function.js
+++ 3. JavaScript/JavaScript Fundamentals/1. ES6 in JavaScript/12. Curring Function in JS/1. Infinte Curring fucntion in JS/Infinite_Curring_function.js
@@ -8,14 +8,17 @@ function Add(a) {
             return function (d) {
                 return function (e) {
                     return function (f) {
-                        return a + b + c + d + e + f;
+                        return function (g) {
+                            return a + b + c + d + e + f + g;
+                        }
                     }
                 }
             }
         }
     }
 }
-// console.log(Add(1)(2)(3)(4)(5)(6)); // 21
+// console.log(Add(1)(2)(3)(4)(5)(6)(1)); // 22
+
 
 // above function mein agar infinte run karna hoga toh bahut hectik kaam hai so let's below exam..
 
