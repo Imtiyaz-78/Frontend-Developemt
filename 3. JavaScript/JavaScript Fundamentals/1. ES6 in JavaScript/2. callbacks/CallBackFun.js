8dcefe383f8ca2708c2a66a78e5205f55ce7e0c9
imtiyaz-78 <imtiyazmd803@gmail.com>
2024-02-20 19:00:01 +0530

    To use Inbuild All UI Material

--- 3. JavaScript/JavaScript Fundamentals/1. ES6 in JavaScript/2. callbacks/CallBackFun.js
+++ 3. JavaScript/JavaScript Fundamentals/1. ES6 in JavaScript/2. callbacks/CallBackFun.js
@@ -9,10 +9,10 @@ function divide(a, b) {
     return a / b
 }
 
-function calculate(num1, num2, callbak){
-     return callbak(num1, num2) 
+function calculate(num1, num2, callbak) {
+    return callbak(num1, num2)
 }
 
 
-console.log(calculate(4 , 2, add));    // 6
-console.log(calculate(4 , 2, divide)); // 2
+console.log(calculate(4, 2, add));    // 6
+console.log(calculate(4, 2, divide)); // 2
