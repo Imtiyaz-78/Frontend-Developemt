6b7d626fce9ab362ec2b5438cd2c8e054a32874c
MD IMTIYAZ <64704175+Imtiyaz-78@users.noreply.github.com>
2025-03-10 11:34:33 +0530

    Difference b/w map() and forEach() JS method

--- 3. JavaScript/JavaScript Fundamentals/1. Tricky Quesdtion of JS/1. map vs forEach/map_forEach.js
+++ 3. JavaScript/JavaScript Fundamentals/1. Tricky Quesdtion of JS/1. map vs forEach/map_forEach.js
@@ -4,16 +4,22 @@
 
 /*
 map() ==> In JavaScript, the map() method is a built-in function for arrays that return or creates a new array 
-          It doesn’t modify the original array but instead returns a new one.
+          It doesn’t modify the original array but instead returns a new array.
 
 */
-const arr = [1, 2, 3, 4, 5, 6];
-const resultWithMap = arr.map((item) => {
-    return item * item;
-})
-console.log(resultWithMap); // [1, 4, 9, 16]
 
-let resultWithForEach = arr.forEach((items) => {
-    console.log(items * items);
-})
+const arr = [1, 2, 3, 4, 5, 6];
+const resultWithMap = arr.map(item => {
+  return item * item;
+});
+console.log(resultWithMap); // [1, 4, 9, 16, 25, 36]
+console.log(arr); // [1, 2, 3, 4, 5, 6]
+
+
+const arr2 = [1, 2, 3, 4];
+let resultWithForEach = arr2.forEach(items => {
+  console.log(items * items);
+});
+
 console.log(resultWithForEach); // undefined
+console.log(arr2); // [1, 2, 3, 4]
